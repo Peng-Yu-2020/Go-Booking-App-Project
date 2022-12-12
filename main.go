21acd7acd384244d02cea4c295b5915cb937a59c
Peng-Yu-2020 <Peng_Yu@student.uml.edu>
2022-12-11 20:32:22 -0500

    upload files

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import ("fmt"
+		"strings")
+
+func main()  {
+	// var conferenceName = "Go Conference"
+	conferenceName := "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets uint= 50
+	fmt.Printf("conferenceName is %T, conferenceTickets is %T, and remainingTickets is %T \n", conferenceName, conferenceTickets, remainingTickets)
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+
+	// var bookings [50]string // definition for array
+	// var bookings []string // definition for slice
+	bookings := []string{}
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
+
+		fmt.Printf("Enter your first name:")
+		fmt.Scan(&firstName)
+		fmt.Printf("Enter your last name:")
+		fmt.Scan(&lastName)
+		fmt.Printf("Enter your email address:")
+		fmt.Scan(&email)
+		fmt.Printf("Enter number of tickets:")
+		fmt.Scan(&userTickets)
+		if userTickets <= remainingTickets {
+			remainingTickets = remainingTickets - userTickets
+			fmt.Println("------------")
+
+			// bookings[0] = firstName + " " + lastName // used in array case
+			bookings = append(bookings, firstName + " " + lastName)
+			// fmt.Printf("The whole slice: %v \n", bookings)
+			// fmt.Printf("The first value: %v \n", bookings[0])
+			// fmt.Printf("Slice type: %T\n", bookings)
+			// fmt.Printf("Slice length: %v\n", len(bookings))
+
+			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v \n", firstName, lastName, userTickets, email)
+			fmt.Printf("%v tickets remaining for %v \n", remainingTickets, conferenceName)
+
+			firstNames := []string{}
+			for _, booking := range bookings {
+				var names = strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
+			fmt.Printf("The first names of bookings are: %v \n", firstNames)
+			
+			if remainingTickets == 0 {
+				fmt.Println("Our conference is booked out. Come back next year")
+				break
+			} 
+		} else {
+			fmt.Printf("We only have %v tickets remaining, so you can't book %v tickets \n", remainingTickets, userTickets)
+		}
+		
+	}
+	
+}
