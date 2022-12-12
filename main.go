5def74d7d452255b86bd6bbc5afac7a7bc5c0bb6
Peng-Yu-2020 <Peng_Yu@student.uml.edu>
2022-12-11 21:06:28 -0500

    add input validation

--- main.go
+++ main.go
@@ -31,7 +31,11 @@ func main()  {
 		fmt.Scan(&email)
 		fmt.Printf("Enter number of tickets:")
 		fmt.Scan(&userTickets)
-		if userTickets <= remainingTickets {
+
+		isValidName := len(firstName) >= 2 && len(lastName) >= 2
+		isValidEmail := strings.Contains(email, "@")
+		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+		if  isValidName && isValidEmail && isValidTicketNumber {
 			remainingTickets = remainingTickets - userTickets
 			fmt.Println("------------")
 
@@ -57,7 +61,7 @@ func main()  {
 				break
 			} 
 		} else {
-			fmt.Printf("We only have %v tickets remaining, so you can't book %v tickets \n", remainingTickets, userTickets)
+			fmt.Printf("Your input data are not valid, try again")
 		}
 		
 	}
